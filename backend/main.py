3a64f8f600b627e3a17169222ad20f224fe31f98
Dr.Ahmed Refaat (Sales Department) <a.refaat@lotuspharmacies.com>
2025-12-21 11:44:24 +0200

    Enable CORS and connect dashboard to backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, Query
 from datetime import date
 from db import get_connection
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # مؤقتًا للتجربة
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # ==============================
 #   Create FastAPI App
@@ -78,3 +88,75 @@ def sales_by_date(
         "grand_total": grand_total,
         "days": days
     }
+
+@app.get("/dashboard/summary")
+def dashboard_summary():
+    db = get_connection()
+    cur = db.cursor()
+
+    # مبيعات اليوم
+    cur.execute("""
+        SELECT ISNULL(SUM(NetAmount), 0)
+        FROM Invoices
+        WHERE CAST(InvoiceDate AS DATE) = CAST(GETDATE() AS DATE)
+    """)
+    today_sales = cur.fetchone()[0]
+
+    # عدد فواتير اليوم
+    cur.execute("""
+        SELECT COUNT(*)
+        FROM Invoices
+        WHERE CAST(InvoiceDate AS DATE) = CAST(GETDATE() AS DATE)
+    """)
+    invoice_count = cur.fetchone()[0]
+
+    returns_total = 0
+
+    net_sales = float(today_sales) - float(returns_total)
+
+
+    
+
+    db.close()
+
+   
+   
+
+
+
+    return {
+        "date": str(date.today()),
+        "today_sales": float(today_sales),
+        "invoice_count": invoice_count,
+        "returns_total": float(returns_total),
+        "net_sales": net_sales
+    }
+
+@app.get("/dashboard/sales-last-7-days")
+def sales_last_7_days():
+    db = get_connection()
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT 
+            CAST(InvoiceDate AS DATE) AS SaleDate,
+            ISNULL(SUM(NetAmount), 0) AS TotalSales
+        FROM Invoices
+        WHERE InvoiceDate >= DATEADD(DAY, -6, CAST(GETDATE() AS DATE))
+        GROUP BY CAST(InvoiceDate AS DATE)
+        ORDER BY SaleDate
+    """)
+
+    rows = cur.fetchall()
+    db.close()
+
+    result = []
+    for row in rows:
+        result.append({
+            "date": str(row[0]),
+            "total": float(row[1])
+        })
+
+    return result
+
+
